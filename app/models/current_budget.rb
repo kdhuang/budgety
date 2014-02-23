3206d19c269cf094335fe238e8f10f3a703662ba
Kevin Huang <khuang@ubuntu.(none)>
2014-02-23 01:40:00 -0800

    BIG BUG FIX. recalculation was taking transactions..even across users. Now it only does recalculates for current user

--- app/models/current_budget.rb
+++ app/models/current_budget.rb
@@ -2,7 +2,7 @@ class CurrentBudget < ActiveRecord::Base
 	belongs_to :user
 
 	def self.recalculate(user, new_budget)
-		current_month_transactions = Transaction.all.group_by { |t| t.date.beginning_of_month }[Date.current.beginning_of_month]
+		current_month_transactions = Transaction.find_all_by_user_id(user).group_by { |t| t.date.beginning_of_month }[Date.current.beginning_of_month]
 		current = CurrentBudget.find_by_user_id(user.id)
 		current.budget = new_budget
 		if current_month_transactions != nil
